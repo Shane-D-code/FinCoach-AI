7e89a304a54fa4c35f83e5bf3c711a2492991fe2
Shane-D-code <s.shantanu.ai@gmail.com>
2025-10-13 21:24:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    chunkSizeWarningLimit: 2000, // in KB, adjust as needed
   },
-});
+})
